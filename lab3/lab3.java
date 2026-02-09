d196c7e844f7572cb93161a36b6304157b1dbaf6
Karakoz <bolatkarakoz710@gmail.com>
2026-02-09 11:20:38 +0500

    lab3 1

--- lab3/lab3.java
+++ lab3/lab3.java
@@ -1,4 +1,91 @@
-package PACKAGE_NAME;
+//public class lab3
+import java.util.Scanner;
 
-public class lab3 {
+public class Main {
+    public static void main(String[] args) {
+
+        // Создание объекта Scanner для ввода данных
+        Scanner scanner = new Scanner(System.in);
+
+        // Ввод длины прямоугольника
+        System.out.print("x тың мәні: ");
+        double x= scanner.nextDouble();
+        //1 есеп
+        // double f1=Math.cbrt(Math.exp(2*x)) - Math.abs(Math.sin(Math.PI * x / (x + 2.0/3.0))) + 1.7;
+
+        //2 есеп
+        // double f1=Math.log(Math.abs(x - 1.5))
+        //+ Math.pow(Math.abs(x), 4.0/5.0)
+        //+ Math.cbrt(Math.exp(3*x) + 4);
+
+        //3
+        //double f1=Math.tan((5.0/3.0)*x)
+        // + Math.log(Math.abs(2*x + 5))
+        //+ 1.8;
+
+        //4
+        //double f1=Math.cos(5+x)+ Math.pow(Math.PI + Math.abs(2-x),7.0);
+
+        //5
+        //double f1=Math.pow(x,5)+ Math.log(Math.sqrt(Math.PI + Math.abs(5 - x)));
+
+        //6
+        // int xi = (int)x;
+        // double f1=Math.pow(x,7) + xi / 2+ xi % 5+ Math.exp(5*x);
+
+        //7
+        //int xi = (int)x;
+        //double f1=xi % 6+ Math.pow(x,3) * Math.log(x*x);
+
+        //8
+        //double f1=Math.pow(x*7, 5)+ Math.sin(Math.pow(x,4))+ Math.tan(Math.pow(x,3));
+
+        //9
+        //double f1= Math.pow(Math.exp(3*x) + 7, 4)+ Math.cos(Math.pow(x,5))+ Math.log(Math.abs(3*x + 7));
+
+        //10
+        // double f1= Math.log(5*x)+ Math.pow(x,5) * Math.sin(Math.pow(x,3))+ Math.tan(Math.pow(x,4));
+
+        //11
+        //12
+        //Қосымша есептер
+        //13
+        //double f1 = Math.log(5*x) + Math.pow(x, 5) * Math.tan(Math.pow(x, 3)) + Math.sin(Math.pow(x, 4));
+        //14
+        //double f1 = Math.pow(x + 2, 3)+ Math.sin(Math.pow(x, 2))
+        //+ Math.log(Math.abs(2*x + 1));
+
+        //15
+        //double f1 = Math.exp(x)+ Math.cos(Math.pow(x, 3))- Math.sqrt(Math.abs(x - 4));
+
+        //16
+        //double f1 = Math.pow(3*x, 2)+ Math.tan(x) + Math.log(Math.abs(x + 7));
+
+//17
+//double f1 = Math.sin(x)+ Math.cos(Math.pow(x, 2)) + Math.exp(2*x);
+
+//18
+//double f1 = Math.pow(x + 3, 2)+ Math.sin(Math.pow(x, 2))+ Math.log(Math.abs(x + 1));
+
+//19
+//double f1 = Math.sin(x)+ Math.cos(Math.pow(x, 2))+ Math.exp(2*x)- Math.sqrt(Math.abs(x));
+
+//20
+//double f1 = Math.pow(x, 4)+ Math.sin(Math.pow(x, 3))+ Math.log(Math.abs(x + 6))+ Math.cos(x);
+
+//21
+//double f1 = Math.exp(2*x) + Math.sin(Math.pow(x, 3))- Math.sqrt(Math.abs(x + 5));
+
+//22
+//double f1 = Math.pow(5*x, 2)+ Math.tan(x)+ Math.cos(Math.pow(x, 2));
+
+        //23
+        //double f1 = Math.sin(x)+ Math.exp(x)+ Math.log(Math.abs(x + 2))- Math.sqrt(Math.abs(3*x));
+
+//24
+//double f1 = Math.pow(x, 3)+ Math.cos(Math.pow(x, 4)) + Math.sin(Math.pow(x, 2))+ Math.exp(x);
+
+
+        // Вывод результатов
+        System.out.println("f=: " + f1);{
 }
